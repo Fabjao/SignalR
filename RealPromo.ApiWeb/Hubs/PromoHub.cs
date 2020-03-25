7ced1131e2ccbce6ff497319c7c19618da8ae6e7
Fábio Gonçalves <fabiobasquete4@hotmail.com>
2020-03-25 12:16:02 -0300

    Versão signalR com API e Front JavaScript ok

---
+++ RealPromo.ApiWeb/Hubs/PromoHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealPromo.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealPromo.ApiWeb.Hubs
+{
+    public class PromoHub : Hub
+    {
+        /*
+         * Cliente - JS/C#/Java
+         * RPC 
+         * Cliente -> Hub
+         * 
+          */
+
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+
+            /* onde pode por a sua logica
+             * Banco de dados
+             * Queue/Scheduler
+             * Notificar o usuario (SignalR)
+             */
+
+            await Clients.Caller.SendAsync("CadastradoSucesso");
+            await Clients.Others.SendAsync("ReceberPromocao", promocao);
+
+        }
+
+    }
+}
